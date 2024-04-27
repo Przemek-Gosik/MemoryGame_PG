86bf11553fccb9817091293fa69fa87cd194a89f
Pawel Ciszewski <kardahim@gmail.com>
2024-04-27 11:31:54 +0200

    Initial Commit

---
+++ MemoryGame_PG/MemoryGame_PGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame_PGApp.swift
+//  MemoryGame_PG
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGame_PGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
